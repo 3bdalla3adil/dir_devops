3bf606b2952d0f55f43b3c25e054047113a0cd9f
abdulla bashir <abdullagozer@gmail.com>
2024-08-06 13:40:19 +0300

    [MOD]:removed odoo_devops.py

--- odoo_devops.py
+++ odoo_devops.py
@@ -7,10 +7,3 @@ from odoo import models, tools, _
 class BaseModel(models.AbstractModel):
     _inherit = 'base'
 
-    # override master create function
-    @api.model_create_multi
-    def create(self, vals_list):
-        
-	# Add Your custom here .
-
-        return super(BaseModel, self).create(vals_list)
